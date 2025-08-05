4f21055f48d207e53cf0a45933f88131b470031b
Aayush <ayushkokate1@gmail.com>
2025-08-05 18:19:02 +0530

    refactor: add status code with response

--- app/api/waitlist/route.ts
+++ app/api/waitlist/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from 'zod'
 import { prismaClient } from "@/lib/db";
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest) {
   const body = await req.json();
   const mail = body.mail
   // zod checkpoint to ensure its a valid mailid
